f5f19ea9b090ef600c55f0ad951d0170497d5276
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2026-08-24 23:54:07 +0530

    going from inmemory uploading a large file instead using streaming method and process those part
    Signed-off-by: Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>

--- uploader/handler.go
+++ uploader/handler.go
@@ -1,18 +1,23 @@
-package main	
+package main
 
 import (
 	"fmt"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
+//isse with this is it buffers the file in memory
+// large file stored in temporary files avoiding IO overhead
 func uploadHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Entering into file uploading")
 	//parse our multipart form so that max limit of file is 10 MB
+	//bitwise left--10*2^20--2^20--1 MB--10 MB
 	r.ParseMultipartForm(10<<20)
 	 // FormFile returns the first file for the given key `aniFile"
-	// curl -X POST -F "anifile=@C:\Users\Aniket\Downloads\deep learning interviews.pdf" http://localhost:8080/
+	// curl -X POST -F "anifile=@c:\Users\Aniket\Downloads\TUN_6417.JPG" http://localhost:8080/
 
 	file,header,err := r.FormFile("anifile")
 	if err!=nil{
@@ -40,4 +45,50 @@ func uploadHandler(w http.ResponseWriter, r *http.Request){
 		http.Error(w, "Error saving the file", http.StatusInternalServerError)
 		return
 	}
+}
+
+func uploadStreamingHandler(w http.ResponseWriter, r* http.Request){
+	fmt.Println("Entering into file uploading using streaming instead of directly putting the file in memory")
+	const MAX_UPLOAD_SIZE = 1<<30 // 1GB max size(1*2^30)
+	const MAX_BUFFER_SIZE = 64<<10 //64 KB (64*2^10)
+	//limit the request body to max upload size
+	r.Body = http.MaxBytesReader(w,r.Body,MAX_UPLOAD_SIZE)
+	contentType := r.Header.Get("Content-Type")
+	if contentType==""{
+        http.Error(w,"content-type is not set in header",http.StatusBadRequest)
+		return
+	}
+	reader,err:=r.MultipartReader()
+    if err!=nil{
+		http.Error(w,"unable to create multi part reader",http.StatusBadRequest)
+		return
+	}
+	//process each prt of request
+	// #avoiding entire request buffer in memory
+	for{
+		next_part,err:=reader.NextPart()
+		//no parts left for processing
+		if err==io.EOF{
+			break
+		}
+		if err!=nil{
+           http.Error(w,"error reading part",http.StatusBadRequest)
+		   return 
+		return
+		}
+		if next_part.FileName()==""{
+			continue
+		}
+		//processing file part
+		if err:=processPart(next_part);err!=nil{
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+		   return 
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Println("Upload completed successfully")
+}
+
+func processPart(p *multipart.Part)error{
+	return nil
 }
