874a16cf31a5ee9d0d18aef91f17dacbba00dc45
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2026-08-28 16:15:06 +0530

    adding go routine to communicate with channels asynchronously with 4 parallel workers also using offset and seek prevent file reading and data corruption of same chunks
    Signed-off-by: Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>

--- uploader/Chunker/chunker.go
+++ uploader/Chunker/chunker.go
@@ -93,5 +93,85 @@ func(c *DefaultFileChunker) ChunkLargeFileChunkFile(filepath string)([]ChunkMeta
 	if fileinfo.Size()%int64(c.chunksize)!=0{
 		num_of_chunks++
 	}
+	//creating channel to communicate between goroutine
+	indexchan:=make(chan int,num_of_chunks)
+	chunkchan :=make(chan ChunkMetadata,num_of_chunks)
+	errchan :=make(chan error,num_of_chunks)
+
+	//populate index channel with chunk indexes
+	for i:=0;i<num_of_chunks;i++{
+		indexchan<-i
+	}
+	close(indexchan)
+	//processing chunks in parallel 4 worker
+	for i:=0;i<4;i++{
+		wg.Add(1)
+		go func(){
+           for idx:= range indexchan{
+			//4 parallel worker can ovverite same chunk of file
+			//so chunk size say 10 MB
+			//so worker 0--0,worker1--10 , worker2--20
+			//Without this calculation, a worker dont know where its chunk begins.
+			  offset:=int64(idx)*int64(c.chunksize)
+			  //bsically buffer size for eacvh chunk
+			  buffer:=make([]byte,c.chunksize)
+              //seek to appropriate pos in the file as 4 worker so it will move the reading cursor to 0 before specific worker strt reading
+			  file.Seek(offset,0)
+              //read chunksize bytes from file into buffer
+				bytesReader,err:=file.Read(buffer)
+				if err!=nil && err!=io.EOF{
+					errchan<-err
+					return 
+				}
+				//bytesread==0 means EOF is reached
+				if bytesReader==0{
+					break;
+				}
+				if bytesReader>0{
+					hashr := md5.Sum(buffer[:bytesReader])
+					hashstring := hex.EncodeToString(hashr[:])
+
+					//create the chunk file name
+					chunkfileName := fmt.Sprintf("%s.chunk.%d",filepath,idx)
+
+					nextchunkfile,err:=os.Create(chunkfileName)
+					if err!=nil{
+						errchan<-err
+						return
+					}
+					//writing the data into new chunkfike
+					_,err=nextchunkfile.Write(buffer[:bytesReader])
+					if err!=nil{
+						errchan<-err
+						return
+					}
+					//append data into chunk and update metadata
+					chunk := ChunkMetadata{FileName: chunkfileName,
+					MD5Hash: hashstring,
+					Index: idx,
+					}
+                    mu.lock()
+					chunks = append(chunks, chunk)
+					mu.unlock()
+
+					defer nextchunkfile.Close()
+					chunkchan<-chunk
+				}
+			  
+		   }
+		}()
+	}
+	//wait for all goroutine tot finish
+	go func(){
+     wg.wait()
+	 close(chunkchan)
+	 close(errchan)
+	}()
+
+	for err:= range(errchan){
+		if err!=nil{
+			return nil,err
+		}
+	}
 	return  chunks,nil
 }
