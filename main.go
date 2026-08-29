0eeecaefc1393bec6b7958fcdd6c0897b8efa619
Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>
2026-08-30 01:33:31 +0530

    updating main to consume chunkledUploadHandler
    Signed-off-by: Aniket Bose <97694136+Ani-Bos@users.noreply.github.com>

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"net/http"
 	"log"
 	"video_processing_pipeline/uploader"
+	"video_processing_pipeline/uploader/chunkersse"
 )
 
 func main() {
+	manager:=chunkersse.NewChunkedUploadManager("uploads")
+	handler:=uploader.NewHandlerStruct(manager)
+	http.HandleFunc("/upload/init",handler.HandleStartUpload)
+	http.HandleFunc("/upload/chunk",handler.HandleUploadChunks)
+	http.HandleFunc("/upload/complete",handler.HandleCompleteUpload)
+	http.HandleFunc("/upload/status",handler.HandleGetStatusUpload)
 	http.HandleFunc("/",uploader.UploadHandler)
 	http.HandleFunc("/upload",uploader.UploadStreamingHandler)
 	// http.HandleFunc("/handleUpload",uploader.UploadChunkedHandler)
